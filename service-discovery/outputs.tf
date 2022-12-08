e179b66849cef1d42cd7c94ce20ab642299ec098
Yao <0xyao.zh@gmail.com>
2022-12-08 19:13:51 +1300

    Update outputs.tf

--- service-discovery/outputs.tf
+++ service-discovery/outputs.tf
@@ -2,3 +2,18 @@ output "webserver_lb_dns" {
   description = "Webserver ALB DSN name"
   value       = aws_lb.webserver_lb.dns_name
 }
+
+output "webserver_ns_name" {
+  description = "Webserver private DNS namespace name"
+  value       = aws_service_discovery_private_dns_namespace.webserver_service_discovery_ns.name
+}
+
+output "webserver_service_1_name" {
+  description = "Webserver service 1 name"
+  value       = aws_service_discovery_service.web_service_1.name
+}
+
+output "webserver_service_2_name" {
+  description = "Webserver service 2 name"
+  value       = aws_service_discovery_service.web_service_2.name
+}
